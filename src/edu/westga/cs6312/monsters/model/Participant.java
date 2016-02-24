ef0aff41db932899a9b0e39990b6ed93dbb34526
jim perry jr - m.s.a.c.s. <jperry11@my.westga.edu>
2016-02-24 03:20:28 -0500

    + Participant class, int healthcredits added

---
+++ src/edu/westga/cs6312/monsters/model/Participant.java
@@ -0,0 +1,14 @@
+package edu.westga.cs6312.monsters.model;
+
+/**
+ * Midterm  
+ * @author 	cs6312
+ * @author 	jim perry, jr.
+ * @version	02.24.2016
+ */
+public class Participant {
+	
+	int healthCredits;
+	
+	
+}
