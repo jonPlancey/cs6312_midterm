79b0a850e4eb8105fc4ecab7776a56c7200b6861
jim perry jr - m.s.a.c.s. <jperry11@my.westga.edu>
2016-02-24 03:18:40 -0500

    + MonsterDriver with man and run() added

--- src/edu/westga/cs6312/monsters/controller/MonsterDriver.java
+++ src/edu/westga/cs6312/monsters/controller/MonsterDriver.java
@@ -1,5 +1,22 @@
 package edu.westga.cs6312.monsters.controller;
 
+/**
+ * Midterm  
+ * @author 	cs6312
+ * @author 	jim perry, jr.
+ * @version	02.24.2016
+ */
 public class MonsterDriver {
+
+	/**
+	 * This method is the entry point of the application
+	 * @param args Command line arguments, not used
+	 */ 
+	public static void main(String[] args) {
+		GameBoard game = new GameBoard(); 
+		MonsterTUI gui = new MonsterTUI(game);
+		gui.run();
+
+	}
 
 }
