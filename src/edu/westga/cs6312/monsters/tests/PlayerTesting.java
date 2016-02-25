7aeac7d0bd929cbc38bea0c8a90a961e10b4d05e
jim perry jr - m.s.a.c.s. <jperry11@my.westga.edu>
2016-02-24 23:55:45 -0500

    # class PlayerTesting, specific test methods added

--- src/edu/westga/cs6312/monsters/tests/PlayerTesting.java
+++ src/edu/westga/cs6312/monsters/tests/PlayerTesting.java
@@ -1,23 +1,42 @@
 package edu.westga.cs6312.monsters.tests;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
+
+
+/**
+ * Midterm Unit Test 
+ * @author 	cs6312
+ * @author 	jim perry, jr.
+ * @version	02.24.2016
+ */
 public class PlayerTesting {
-
+	
+	/** 
+	 * tests to confirm constructor and
+	 * toString methods function correctly
+	 */	
 	@Test
-	public void testFight() {
+	public void WhenPlayerIsCreated() {
 		fail("Not yet implemented");
 	}
-
+	
+	/** 
+	 * tests to confirm the accessor 
+	 * method functions correctly
+	 */	
 	@Test
-	public void testPlayer() {
+	public void WhenPlayerGetHealthCredits() {
 		fail("Not yet implemented");
 	}
-
+	
+	/** 
+	 * tests to confirm that the 
+	 * fight method functions correctly
+	 */	
 	@Test
-	public void testGetHealthCredits() {
+	public void WhenPlayerFights() {
 		fail("Not yet implemented");
 	}
 
--- src/edu/westga/cs6312/monsters/tests/RoomTesting.java
+++ src/edu/westga/cs6312/monsters/tests/RoomTesting.java
@@ -1,9 +1,16 @@
 package edu.westga.cs6312.monsters.tests;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
+
+
+/**
+ * Midterm Unit Test 
+ * @author 	cs6312
+ * @author 	jim perry, jr.
+ * @version	02.24.2016
+ */
 public class RoomTesting {
 
 	@Test
