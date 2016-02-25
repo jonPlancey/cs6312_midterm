899c98a8a5bb05984cda36037ed233d28e213b23
jim perry jr - m.s.a.c.s. <jperry11@my.westga.edu>
2016-02-24 20:44:53 -0500

    + Room class added, only to eliminate errors

---
+++ src/edu/westga/cs6312/monsters/model/Room.java
@@ -0,0 +1,15 @@
+package edu.westga.cs6312.monsters.model;
+
+/**
+ * Midterm  
+ * @author 	cs6312
+ * @author 	jim perry, jr.
+ * @version	02.24.2016
+ */
+public class Room {
+
+	public Room() {
+		
+	}
+
+}
