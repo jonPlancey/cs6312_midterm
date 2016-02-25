8b0a8e12f4cd78ac4ee9a970f16283468a4d33f1
jim perry jr - m.s.a.c.s. <jperry11@my.westga.edu>
2016-02-24 23:49:46 -0500

    + test classes created for Player, KickingMonster, Room and GameBoard

---
+++ src/edu/westga/cs6312/monsters/tests/GameBoardTesting.java
@@ -0,0 +1,14 @@
+package edu.westga.cs6312.monsters.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class GameBoardTesting {
+
+	@Test
+	public void testGetCurrentRoom() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/edu/westga/cs6312/monsters/tests/KickingMonsterTesting.java
@@ -0,0 +1,24 @@
+package edu.westga.cs6312.monsters.tests;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class KickingMonsterTesting {
+
+	@Test
+	public void testFight() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testKickingMonster() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetHealthCredits() {
+		fail("Not yet implemented");
+	}
+
+}
