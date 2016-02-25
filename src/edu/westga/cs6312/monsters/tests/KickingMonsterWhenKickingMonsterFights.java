a1c16dfeb7f69888ac7fbbbacb2c55706bda7fa1
jim perry jr - m.s.a.c.s. <jperry11@my.westga.edu>
2016-02-25 00:31:17 -0500

    # kickingMonster unit tests changed

--- src/edu/westga/cs6312/monsters/tests/KickingMonsterWhenKickingMonsterFights.java
+++ src/edu/westga/cs6312/monsters/tests/KickingMonsterWhenKickingMonsterFights.java
@@ -1,13 +1,25 @@
 package edu.westga.cs6312.monsters.tests;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
+
+
+/**
+ * Midterm Unit Test 
+ * @author 	cs6312
+ * @author 	jim perry, jr.
+ * @version	02.24.2016
+ */
 public class KickingMonsterWhenKickingMonsterFights {
 
+	
+	/** 
+	 * tests to confirm that the 
+	 * fight method functions correctly
+	 */	
 	@Test
-	public void testFight() {
+	public void testWhenKickingMonsterFights() {
 		fail("Not yet implemented");
 	}
 
--- src/edu/westga/cs6312/monsters/tests/KickingMonsterWhenKickingMonsterIsCreated.java
+++ src/edu/westga/cs6312/monsters/tests/KickingMonsterWhenKickingMonsterIsCreated.java
@@ -1,18 +1,35 @@
 package edu.westga.cs6312.monsters.tests;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
+
+
+/**
+ * Midterm Unit Test 
+ * @author 	cs6312
+ * @author 	jim perry, jr.
+ * @version	02.24.2016
+ */
 public class KickingMonsterWhenKickingMonsterIsCreated {
 
+	
+	/** 
+	 * tests to confirm toString 
+	 * methods function correctly
+	 */	
 	@Test
 	public void testToString() {
 		fail("Not yet implemented");
 	}
-
+	
+	
+	/** 
+	 * tests to confirm constructor 
+	 *  methods function correctly
+	 */	
 	@Test
-	public void testKickingMonster() {
+	public void testWhenKickingMonsterIsCreated() {
 		fail("Not yet implemented");
 	}
 
