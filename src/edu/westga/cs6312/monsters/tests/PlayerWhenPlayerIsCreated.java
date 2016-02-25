bfa57172649315894ebf260ad9c126aaa1e6a317
jim perry jr - m.s.a.c.s. <jperry11@my.westga.edu>
2016-02-25 00:27:19 -0500

    # updated test method for player

--- src/edu/westga/cs6312/monsters/tests/PlayerWhenPlayerIsCreated.java
+++ src/edu/westga/cs6312/monsters/tests/PlayerWhenPlayerIsCreated.java
@@ -4,15 +4,31 @@ import static org.junit.Assert.*;
 
 import org.junit.Test;
 
+
+
+/**
+ * Midterm Unit Test 
+ * @author 	cs6312
+ * @author 	jim perry, jr.
+ * @version	02.24.2016
+ */
 public class PlayerWhenPlayerIsCreated {
 
+	/** 
+	 * tests to confirm toString 
+	 * methods function correctly
+	 */	
 	@Test
 	public void testToString() {
 		fail("Not yet implemented");
 	}
-
+	
+	/** 
+	 * tests to confirm constructor and
+	 * toString methods function correctly
+	 */	
 	@Test
-	public void testPlayer() {
+	public void testWhenPlayerIsCreated() {
 		fail("Not yet implemented");
 	}
 
