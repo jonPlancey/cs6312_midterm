b4aa0dac60880370864b68878f0552695968c066
jim perry jr - m.s.a.c.s. <jperry11@my.westga.edu>
2016-02-25 00:32:29 -0500

    # unit tests for room changed

--- src/edu/westga/cs6312/monsters/tests/RoomWhenRoomGetLocation.java
+++ src/edu/westga/cs6312/monsters/tests/RoomWhenRoomGetLocation.java
@@ -1,14 +1,35 @@
 package edu.westga.cs6312.monsters.tests;
 
 import static org.junit.Assert.*;
-
 import org.junit.Test;
 
-public class RoomWhenRoomGetLocation {
 
+
+/**
+ * Midterm Unit Test 
+ * @author 	cs6312
+ * @author 	jim perry, jr.
+ * @version	02.24.2016
+ */
+public class RoomWhenRoomGetLocation {
+	
+	/** 
+	 * tests to confirm the accessor
+	 *  method functions correctly. 
+	 */	
 	@Test
-	public void testGetLocation() {
+	public void testWhenRoomGetLocation01() {
 		fail("Not yet implemented");
 	}
+	
+	
+	/** 
+	 * tests to confirm the accessor
+	 *  method functions correctly. 
+	 */	
+	@Test
+	public void testWhenRoomGetLocation02() {
+		fail("Not yet implemented");
+	}	
 
 }
